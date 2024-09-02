b9208b9ecd66aeab4cde57447e2a4134fd2b5814
Roberto <ro.huaitro@duocuc.cl>
2024-09-02 19:30:40 -0400

    RH - Se agrega login desde localStorage desde lista dummy y se filtra por perfil

--- src/app/pages/Inicio/login/login.page.ts
+++ src/app/pages/Inicio/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
+import { User } from 'src/app/model/User';
 
 @Component({
   selector: 'app-login',
@@ -17,18 +18,54 @@ export class LoginPage implements OnInit {
   password:string=''
 
   ngOnInit() {
+    let users: User[] = [
+      {
+        idUser:1,
+        username: 'meko',
+        password: '1234',
+        profile: {
+          id:1,
+          code: 'profe',
+          name: 'profesor'
+        }
+      },
+      {
+        idUser:2,
+        username: 'dartel',
+        password: '1234',
+        profile: {
+          id:2,
+          code: 'alum',
+          name: 'alumno'
+        }
+      }
+  ];
+
+    localStorage.setItem("users",JSON.stringify(users));
   }
 
-  validar(){
-    if (this.nombre=="juanito" && this.password=="123") {
-      console.log("Bienvenido")
-      localStorage.setItem("usuario",this.nombre)
-      this.navCtrl.navigateForward(['/home'])
-    } else {
-      console.log("Usuario/Password Incorrecto")
-      this.presentAlert()
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.validar();
     }
   }
+
+  validar() {
+  
+    let listLogin: User[] = JSON.parse(localStorage.getItem('users') || '[]');
+    let userLogin = listLogin.find(usr => usr.username === this.nombre && usr.password === this.password);
+
+    if (userLogin) {
+        console.log("Bienvenido");
+        localStorage.setItem("usuario", this.nombre);
+        localStorage.setItem("userLogin",JSON.stringify(userLogin));
+        this.navCtrl.navigateForward(['/home']);
+    } else {
+        console.log("Usuario/Password Incorrecto");
+        this.presentAlert();
+    }
+  }
+
 
   async presentAlert() {
     const alert = await this.alertController.create({
