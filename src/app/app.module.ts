4396520d146daba038e60023096ed8a6003d3471
Ivn00 <iva.diazm@duocuc.cl>
2024-09-25 09:47:36 -0300

    Descripción de los cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { InformeModalModule } from './components/modulo/informe-modal/informe-modal.module'; // Importa el módulo, no el componente
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { environment } from 'src/environments/environment.prod';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule} from '@angular/fire/compat'
+
 
 @NgModule({
   declarations: [AppComponent],  // Elimina la declaración de InformeModalComponent
@@ -14,7 +18,9 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    InformeModalModule // Importa el módulo que contiene InformeModalComponent
+    InformeModalModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAPs-tmGK6H6c_IzoTqC_GGpeZRpT9oKWs",
+    authDomain: "ionicregistrapp-a3794.firebaseapp.com",
+    projectId: "ionicregistrapp-a3794",
+    storageBucket: "ionicregistrapp-a3794.appspot.com",
+    messagingSenderId: "367638692718",
+    appId: "1:367638692718:web:a2a26ed9f9eccc04f2377f"
+  },
 };
 
 /*
