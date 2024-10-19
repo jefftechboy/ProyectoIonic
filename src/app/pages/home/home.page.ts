5ee029ee40692153ba046d51084dfed7261cc52d
Jeffry <jeffrypersonal942001@gmail.com>
2024-10-19 01:30:28 -0300

    consulta de asignaturas con seccion dia y hora del alumno, mas la clase en progeso, funcionando

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,6 +4,9 @@ import { MenuController } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
 import { LoginService } from 'src/app/servicios/inicio/login.service';
 import { ClaseActualService } from 'src/app/servicios/clase/clase-actual.service';
+import { AsistenciaAlumnoService } from 'src/app/servicios/asistencia/asistencia-alumno.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -42,7 +45,8 @@ export class HomePage implements OnInit {
     private menuController: MenuController,
     private navController: NavController,
     public aa:LoginService,
-    public hh:ClaseActualService
+    public hh:ClaseActualService,
+    private asignaturaService: AsistenciaAlumnoService
   ) {}
 
   navigateAndClose(path: string) {
@@ -50,17 +54,97 @@ export class HomePage implements OnInit {
       this.menuController.close('end'); // 'end' es el ID del menú, ajusta si es necesario
     });
   }
-  ngOnInit(): void {
-    // Recuperar el nombre de usuario
-    const storedUser = localStorage.getItem('userLogin');
-    if (storedUser) {
-      const userLogin = JSON.parse(storedUser);
-      this.usuario = userLogin.username || '';
-      this.profileCode = userLogin.profile?.code || '';
-    }
+
+  ngOnInit() {
+    this.iterarAsignatura();
+  }
+
+  asignaturas: Observable<any[]>; // Cambia a un observable
+  secciones: Observable<any[]>; // Cambia a un observable
+  alumnos: Observable<any[]>; // Cambia a un observable
+  horario: Observable<any[]>; // Cambia a un observable
+
+  vecesCombinadas: { 
+    alumnoId: string, 
+    asignaturaId: string, 
+    seccionId: string,
+    dia: string,
+    horaInicio: string,
+    horaFin: string }[] = [];
+
+  claseEnProceso: { 
+    alumnoId: string, 
+    asignaturaId: string, 
+    seccionId: string,
+    dia: string,
+    horaInicio: string,
+    horaFin: string }[] = [];
+
+   //Coleccion segun alumno
+   iterarAsignatura() {
+    this.asignaturas = this.hh.TodasLasAsignaturas();
+    this.asignaturas.subscribe(asignaturas => {
+      // Aquí empieza la iteración sobre los valores
+      for (const asignatura of asignaturas) {
+        // Asegúrate de que asignatura.id existe
+        this.secciones = this.hh.SeccionesPorAsignatura(asignatura.id);
+        this.secciones.subscribe(secciones => {
+          // Aquí empieza la iteración sobre los valores
+          for (const seccion of secciones) {
+            // Asegúrate de que asignatura.id existe
+            this.alumnos = this.hh.alumnoPorSeccion(asignatura.id, seccion.id);
+            this.alumnos.subscribe(nombreAlumno => {
+              // Aquí empieza la iteración sobre los valores
+              for (const nombre of nombreAlumno) {
+                // Asegúrate de que asignatura.id existe
+                if (nombre.id === "Roberto Huaitro") {
+                  this.horario = this.hh.horarioAsignatura(asignatura.id, seccion.id);
+                  this.horario.subscribe(horario => {
+                    // Aquí empieza la iteración sobre los valores
+                    for (const hora of horario) {
+                      this.vecesCombinadas.push({
+                        alumnoId: nombre.id,
+                        asignaturaId: seccion.id,
+                        seccionId: asignatura.id,
+                        dia: hora.id,
+                        horaInicio: hora["Hora Inicio"],
+                        horaFin: hora["Hora Fin"]
+                      });
+                    }
+                    // Llama a claseEnProcesoMetodo aquí, después de llenar vecesCombinadas
+                    this.claseEnProcesoMetodo();
+                  });
+                }
+              }
+            });
+          }
+        });
+      }
+    });
   }
   
   
+  claseEnProcesoMetodo() {
+    this.claseEnProceso = []; // Limpiar el array al inicio
+    for (const item of this.vecesCombinadas) {  
+        console.log((item.dia+item.horaInicio+item.horaFin))
+        console.log(this.hh.verificarHorario(item.dia,item.horaInicio,item.horaFin))
+        if (this.hh.verificarHorario(item.dia,item.horaInicio,item.horaFin)) {
+          this.claseEnProceso.push(item);
+        }
+    }
+    /*
+    for (const a of this.claseEnProceso){
+      console.log(a.seccionId)
+    }
+    */
+  }
+  
+  
+
+
+
+
 
 
 }
--- src/app/servicios/clase/clase-actual.service.ts
+++ src/app/servicios/clase/clase-actual.service.ts
@@ -1,43 +1,101 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Firestore } from 'firebase/firestore';
+import { map, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { forkJoin } from 'rxjs';
+import { combineLatest } from 'rxjs';
+import { from } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ClaseActualService {
 
-  constructor() { }
+  constructor(public afs:AngularFirestore) { }
 
-  ClaseEnProceso(){
-    
+  
+  // LISTA DE TODAS LAS ASIGNATURAS  (DEVUELVE EL ID Y SUS ATRIBUTOS)
+  TodasLasAsignaturas(){
+    return this.afs.collection('Asignatura')
+      .snapshotChanges()  // Obtiene los metadatos y datos de la colección
+      .pipe(
+        map(actions => 
+          actions.map(a => {
+            const id = a.payload.doc.id;  // Obtener el ID del documento
+            return { id };  // Retornar un objeto con los datos y el ID
+          })
+        )
+      );
   }
-
-
-
-
-
-
-
-  verificarHora(inputHora: string): boolean {
-    // Suponiendo que el formato de inputHora es "HH:mm"
-    const [horas, minutos] = inputHora.split(':').map(Number);
-    
-    // Crear un objeto Date con la hora ingresada
-    const horaIngresada = new Date();
-    horaIngresada.setHours(horas, minutos, 0, 0);
-  
-    // Obtener la hora actual del sistema
-    const horaActual = new Date();
-  
-    // Comparar las horas
-    return horaIngresada.getTime() === horaActual.getTime();
+  // SECCIONES POR CADA ASIGNATURA (DEVUELVE EL ID Y SUS ATRIBUTOS)
+  SeccionesPorAsignatura(nomAsignatura:string){
+    return this.afs.collection('Asignatura') // Selecciona todas las asignaturas
+    .doc(nomAsignatura)
+    .collection("Seccion")
+    .snapshotChanges()  // Obtiene los metadatos y datos de la colección
+      .pipe(
+        map(actions => 
+          actions.map(a => {
+            const data = a.payload.doc.data() as any;  // Obtener los datos del documento
+            const id = a.payload.doc.id;  // Obtener el ID del documento
+            return { id, ...data };  // Retornar un objeto con los datos y el ID
+          })
+        )
+      );  
+  }
+  // ALUMNOS FILTRADOS POR SECCION
+  alumnoPorSeccion(nomAsignatura:string,codSeccion:string){
+    return this.afs.collection('Asignatura') // Selecciona todas las asignaturas
+    .doc(nomAsignatura)
+    .collection("Seccion")
+    .doc(codSeccion)
+    .collection("Alumno")
+    .snapshotChanges()  // Obtiene los metadatos y datos de la colección
+      .pipe(
+        map(actions => 
+          actions.map(a => {
+            const id = a.payload.doc.id;  // Obtener el ID del documento
+            return { id };  // Retornar un objeto con los datos y el ID
+          })
+        )
+      );  
   }
   
-  // Ejemplo de uso
- // const horaConsulta = "15:30"; // Cambia esto a la hora que deseas verificar
-  // const esLaMismaHora = verificarHora(horaConsulta);
-  // console.log(`¿Es la misma hora? ${esLaMismaHora}`);
+
+  horarioAsignatura(asignaturaNom:string,seccion:string){
+   return this.afs.collection("Asignatura")
+            .doc(asignaturaNom)
+            .collection("Seccion")
+            .doc(seccion)
+            .collection("Horario")
+            .snapshotChanges()  // Obtiene los metadatos y datos de la colección
+            .pipe(
+              map(actions => 
+                actions.map(a => {
+                  const data = a.payload.doc.data() as any;  // Obtener los datos del documento
+                  const id = a.payload.doc.id;  // Obtener el ID del documento
+                  return { id, ...data };  // Retornar un objeto con los datos y el ID
+                })
+              )
+            );  
+  }
+  
+
+  
+  verificarHorario(dia: string, horaInicio: string, horaFin: string): boolean {
+    // Dia De la semana
+    const diasValidos = ['domingo', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'];
+    // Obtener el día actual
+    const diaActual = diasValidos[new Date().getDay()].toLowerCase();
+    // Obtener la hora actual en formato "HH:MM"
+    const horaActual = new Date().toTimeString().slice(0, 5);
+    // Verificar si es el mismo día y si la hora está en el rango
+    return  (dia.toLowerCase() === diaActual ) &&  ( horaActual >= horaInicio ) && ( horaActual <= horaFin ) ;
+  }
   
   
+
+
 }
 
 
