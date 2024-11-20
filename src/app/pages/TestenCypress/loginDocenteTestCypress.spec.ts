e7b2952a994d5024577fc977f74e397d8befb1b8
GabrMonsalve <gabr.monsalve@duocuc.cl>
2024-11-19 21:41:31 -0300

    Añadidos y modificados los tests Cypress

--- src/app/pages/TestenCypress/loginDocenteTestCypress.spec.ts
+++ src/app/pages/TestenCypress/loginDocenteTestCypress.spec.ts
@@ -1,12 +1,12 @@
-describe('Mi primer test', () => {
+describe('Test Login Docente', () => {
     beforeEach(()=>{
         cy.visit('/login')
     });
-    it('visitar pagina',()=>{
+    it('Deberia acceder al menu ingresando datos del Docente',()=>{
         cy.contains('usuario')
         cy.get('input[name = "inputusuario"]').should('be.visible')
-        cy.get('input[name = "inputusuario"]').type('ro.huaitro@duocuc.cl')
-        cy.get('input[name = "inputcontraseña"]').type('1')
+        cy.get('input[name = "inputusuario"]').type('freddy@duocuc.cl')
+        cy.get('input[name = "inputcontraseña"]').type('admin')
         cy.contains('OK').click
         cy.visit('/home')
         cy.url().should('include','/home')
