b4caa6af8955a6676fc2d0b539bb3ee1ba4d7ae5
GabrMonsalve <gabr.monsalve@duocuc.cl>
2024-11-20 20:05:42 -0300

    Agregados los Testing Cypress

--- src/app/pages/TestenCypress/AlumnoAsistenciaTestCypress.spec.ts
+++ src/app/pages/TestenCypress/AlumnoAsistenciaTestCypress.spec.ts
@@ -3,50 +3,21 @@ describe('Test Alumno asistencia', () => {
       cy.visit('/login'); 
     });
   
-    it('Login y verificar acceso al menú lateral con datos de Firestore', () => {
+    it('Login y verificar acceder a las asignaturas del alumno', () => {
     
       cy.contains('usuario');
       cy.get('input[name="inputusuario"]').should('be.visible');
       cy.get('input[name="inputusuario"]').type('ro.huaitro@duocuc.cl');
-      cy.get('input[name="inputcontraseña"]').type('1');
+      cy.get('input[name="inputcontraseña"]').type('tito12');
       cy.contains('OK').click();
   
       
-      cy.visit('/home');
       cy.url().should('include', '/home');
-  
-      
-      cy.intercept('GET', '/path/to/firestore/data').as('getMenuData');
-  
-      // Esperar hasta que la solicitud de Firestore esté completa
-      cy.wait('@getMenuData');
-      cy.get('ion-menu-button').should('be.visible').click(); 
-      cy.wait(1000); // esperar 1 seg para cargar el menu
-     
-      cy.get('ion-menu').should('have.class', 'menu-enabled');
-      cy.get('#MenuCompleto').should('be.visible'); 
-  
-      
-      cy.contains('Lista Cursos Alumnos').should('be.visible'); 
-      cy.contains('Lectura Qr Alumnos').should('be.visible'); 
-  
-     
-      cy.contains('Lista Cursos Alumnos').click(); 
-      cy.url().should('include', '/listadoCursoAlumno'); 
-  
-      // Verificar el contenido de la página de cursos
-      cy.contains('Asistencia Asignaturas').should('be.visible');
-  
-      
-      cy.get('ion-card.asignatura-card').first().click(); 
-  
-     
-      cy.get('ion-modal').should('be.visible');
-      cy.contains('Asistencia').should('be.visible');
-  
-      
-      cy.contains('Cerrar').click();
-      cy.get('ion-modal').should('not.exist');
-    });
+      cy.get('ion-menu-button').click();
+      cy.get('#BotonHome').click(); 
+      cy.url().should('include', '/listadoCursoAlumno');
+      cy.get('ion-card[name="botoncursos"]').first().should('be.visible').wait(1000).click();
+
   });
+});
   
